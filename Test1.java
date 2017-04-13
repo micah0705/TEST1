17348504726f2ddafe3cccad1eca88619bf6801c
micah <micah0705@naver.com>
2017-04-13 14:47:27 +0900

    1.4

--- Test1.java
+++ Test1.java
@@ -4,6 +4,7 @@ class Test1
 	{
 		System.out.println("Hello");
 		System.out.println(" World");
-		System.out.ptintln("  JAVA");
+		System.out.println("  JAVA");
+		System.out.println(" semyeong");
 	}
 }
