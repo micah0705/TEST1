e388f6ffdffe9046a305e70343e3723068ab11ff
micah <micah0705@naver.com>
2017-04-13 15:46:10 +0900

    1.8

--- Test1.java
+++ Test1.java
@@ -6,7 +6,15 @@ class Test1
 		System.out.println(" World");
 		System.out.ptintln("  JAVA");
 		System.out.println("   Programming");
+<<<<<<< HEAD
 
 		//writer : kim tae hee
+=======
+		System.out.println("  JAVA");
+		System.out.println(" semyeong");
+		System.out.println(" computer");
+
+//writer : kim ji sup
+>>>>>>> 354e02b4f68241852e7654ad4cef629b31491f29
 	}
 }
